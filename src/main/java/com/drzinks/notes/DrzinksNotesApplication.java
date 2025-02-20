96930913bd58402f9da322c63c124c01a76063cf
drzinks <you@example.com>
2025-02-20 17:20:31 +0100

    clean draft from spring initializr

---
+++ src/main/java/com/drzinks/notes/DrzinksNotesApplication.java
@@ -0,0 +1,13 @@
+package com.drzinks.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrzinksNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrzinksNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drzinks/notes/DrzinksNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drzinks.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrzinksNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
